6ae5aadc96dbf07fd0b67221d1e0836d2b646132
Peter Okafor <okaforpeterchukwuemeka2005@gmail.com>
2024-04-10 06:01:16 +0100

    xnxnx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+
+import UserAccountNav from "./UserAccountNav";
+import { ThemeToggle } from "./ThemeToggle";
+import { getAuthSession } from "@/lib/nextauth";
+import SignInButton from "./SignInButton";
+
+const Navbar = async () => {
+  const session = await getAuthSession();
+  return (
+    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300  py-2 ">
+      <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
+      <img src="/quiz.png" alt="Quizorithm" className="w-16 h-16 md:w-32 md:h-32" />
+        <Link href={"/"} className="flex items-center gap-2">
+        
+        </Link>
+        <div className="flex items-center">
+          <ThemeToggle className="mr-4" />
+          {session?.user ? (
+            <UserAccountNav user={session.user} />
+          ) : (
+            <SignInButton text={"Sign In"} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar; 
---
+++ src/lib/nextauth.ts
@@ -0,0 +1,78 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { type GetServerSidePropsContext } from "next";
+import {
+  getServerSession,
+  type NextAuthOptions,
+  type DefaultSession,
+} from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/lib/db";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    jwt: async ({ token }) => {
+      const db_user = await prisma.user.findFirst({
+        where: {
+          email: token?.email,
+        },
+      });
+      if (db_user) {
+        token.id = db_user.id;
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!
+      
+      
+      
+      
+      
+      
+      ,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+};
+
+export const getAuthSession = () => {
+  return getServerSession(authOptions);
+}
